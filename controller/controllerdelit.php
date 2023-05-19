504d0aa463ed29544499a936af4e5de4df5d5616
julianderson <101228303+juliandersonSoares@users.noreply.github.com>
2023-05-19 16:59:03 -0300

    Update controllerdelit.php

--- controller/controllerdelit.php
+++ controller/controllerdelit.php
@@ -1,7 +1,17 @@
 <?php
-include('conn.php');
+    include('conn.php');
 
-$deli=$conn->prepare('DELETE FROM cadastro  WHERE id = :id');
-$deli->bindParam(':id', $id);
-$deli->execute();
+    if( isset($_GET['mensagem'])){
+        $id = $_GET['mensagem'];
+   }else{
+        header("Location: ../views/listalogin.php");
+   }
+
+    $deli=$conn->prepare('DELETE FROM cadastrologin  WHERE id = :id');
+    $deli->execute(array(':id'=>$id));
+
+    echo"<script>
+    alert('Deletado!');
+    window.location.href='../views/listalogin.php';
+    </script>";
 ?>
