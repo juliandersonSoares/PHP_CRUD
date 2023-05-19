7efbd70958e470efe680fcae92a77c5d11c4a259
julianderson <101228303+juliandersonSoares@users.noreply.github.com>
2023-05-19 16:59:27 -0300

    Update controllercadastro.php

--- controller/controllercadastro.php
+++ controller/controllercadastro.php
@@ -1,17 +1,18 @@
 <?php
 include('conn.php');
-$nome=$_POST['nome'];
-$senha=$_POST['senha'];
 
-$cadastro=$conn->prepare('INSERT INTO cadastro(nome,senha) VALUES(:nome,:senha)');
+$nome=$_POST["nome"];
+$senha=$_POST["senha"];
+
+$cadastro=$conn->prepare("INSERT INTO cadastrologin(nome,senha) VALUES(:nome,:senha)");
 $cadastro->execute(array(':nome'=>$nome, ':senha'=>$senha));
 
 if ($cadastro==true){
     echo 'cadastradado com sucesso';
 
-echo '<script>
-alert("Cadastro feito!");
-window.location.href="../views/cadastro.php";
-</script>';
+    echo '<script>
+    alert("usuario cadastrado");
+    window.location.href="../views/cadastro.php"
+    </script>';
 }
 ?>
