25e1b1728ab636d36fded54e411c5de90cb02c7b
julianderson <101228303+juliandersonSoares@users.noreply.github.com>
2023-05-22 16:27:12 -0300

    Update cadastro.php

--- views/cadastro.php
+++ views/cadastro.php
@@ -8,9 +8,14 @@
 </head>
 <body>
     <form method="post" action="../controller/controllercadastro.php">
-        <input type="text" placeholder="Digite um nome" required  name=nome/>
-        <input type="password"   placeholder="Digite uma senha" required name=senha/>
+        <input type="text" placeholder="Digite um nome" required name="nome">
+        <br>
+        <br>
+        <input type="password" placeholder="Digite uma senha" required name="senha">
+        <br>
+        <br>
         <button type="submit">cadastrar</button>
+        <button type="submit"><a href="../views/listalogin.php">listar</a></button>
     </form>
 </body>
 </html>
