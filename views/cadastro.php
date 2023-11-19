7d29e65126eb22e232e7408a7bf94c4b22959241
juliandersonSoares <jsoares.4720@gmail.com>
2023-11-18 22:18:07 -0300

    modificação no projeto crud

--- views/cadastro.php
+++ views/cadastro.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
+    <link rel="stylesheet" href="../css/cadastro.css">
+
 <body>
-    <form method="post" action="../controller/controllercadastro.php">
-        <input type="text" placeholder="Digite um nome" required name="nome">
-        <br>
-        <br>
-        <input type="password" placeholder="Digite uma senha" required name="senha">
-        <br>
-        <br>
-        <button type="submit">cadastrar</button>
-        <button type="submit"><a href="../views/listalogin.php">listar</a></button>
-    </form>
+    <div class="conteudo-principal">
+        <div class="conteudo-class">
+            <form method="post" action="../controller/controllercadastro.php">
+                <p>
+                <h1>Cadastro</h1>
+                </p>
+                <p>
+                    <label>Digite um Nome de Usúario</label><br>
+                    <input type="text">
+                </p>
+                <p>
+                    <label>Digite um E-mail</label><br>
+                    <input type="text" required name="email">
+                    <br>
+                </p>
+                <p>
+                    <label>Digite uma Senha</label><br>
+                    <input type="password" required name="senha">
+                    <br>
+                </p>
+                <p>
+                    <button type="submit">cadastrar</button>
+                    <br><br>
+                    <button type="submit"><a href="../views/logar.php">Logar</a></button>
+                </p>
+
+            </form>
+        </div>
+    </div>
+
 </body>
+
 </html>
